f5c4e132c7aeba4d149f8eb09f42cf5f25360851
KiiMOTu <itsssonny@icloud.com>
2026-02-12 00:35:44 +0000

    Initial commit of my dotfiles repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# dotfiles
+
+Save yourself from bash with:
+
+My zsh/Oh My Zsh setup. Run `./install.sh` on a fresh box.
---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "Installing zsh and dependencies..."
+sudo apt install -y zsh git curl
+
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "Installing plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Applying config..."
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cp "$SCRIPT_DIR/.zshrc" ~/.zshrc
+
+echo "Setting zsh as default shell..."
+chsh -s $(which zsh)
+
+echo "Done! Restart your shell or run: exec zsh"
